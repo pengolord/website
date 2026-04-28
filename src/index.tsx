69778dcb48ba0e6221d37fd5d2b7e4240236bfe8
pengo <pbalternates@gmail.com>
2026-04-27 23:14:34 -0600

    feat: install and setup solidjs

---
+++ src/index.tsx
@@ -0,0 +1,12 @@
+/* @refresh reload */
+import { render } from 'solid-js/web';
+import 'solid-devtools';
+
+
+const root = document.getElementById('root');
+render(
+  () => <>
+    <h1>Hello!!</h1>
+  </>,
+  root!
+);
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,12 @@
 import { defineConfig } from 'vite';
+import solidPlugin from 'vite-plugin-solid';
+import devtools from 'solid-devtools/vite';
 
 export default defineConfig({
-  plugins: [],
+  plugins: [
+    devtools(),
+    solidPlugin(),
+  ],
   server: {
     port: 5173,
   },
