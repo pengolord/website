fc7f452797f774dd84361ae298044ddd344b966f
pengo <pbalternates@gmail.com>
2026-04-27 23:18:06 -0600

    feat: install and setup scss

--- src/index.tsx
+++ src/index.tsx
@@ -2,11 +2,12 @@
 import { render } from 'solid-js/web';
 import 'solid-devtools';
 
+import './index.scss';
 
 const root = document.getElementById('root');
 render(
-  () => <>
+  () => <main>
     <h1>Hello!!</h1>
-  </>,
+  </main>,
   root!
 );
